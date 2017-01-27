dc50131408cc653387649f63553b096a196c4fc2
asend <alyndiaye.ag@gmail.com>
2017-01-27 19:58:42 +0000

    initial commit

---
+++ app/src/main/java/com/example/asend/popapr/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.asend.popapr;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    Button b1,b2;
+    EditText ed1,ed2;
+
+    int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button);
+        ed1 = (EditText)findViewById(R.id.editText);
+        ed2 = (EditText)findViewById(R.id.editText2);
+
+        b2 = (Button)findViewById(R.id.button2);
+
+        /*b1.setOnClickListener(new View.OnClickListener() {
+            @Override*/
+
+       // });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    public void Go(View v) {
+        if(ed1.getText().toString().equals("admin") &&
+                ed2.getText().toString().equals("admin")) {
+            /*Toast.makeText(getApplicationContext(),
+                    "Redirecting...",Toast.LENGTH_SHORT).show();*/
+            //on creer une nouvelle intent on definit la class de depart ici this et la class d'arrivé ici UserList
+            Intent intent=new Intent(this,UserList.class);
+            //intent.putExtra("prenom", ed1.getText().toString());
+            //on lance l'intent, cela a pour effet de stoper l'activité courante et lancer une autre activite ici SecondActivite
+            startActivity(intent);
+
+        }else{
+            Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+            counter--;
+
+            if (counter == 0) {
+                b1.setEnabled(false);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/asend/popapr/UserList.java
@@ -0,0 +1,34 @@
+package com.example.asend.popapr;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by asend on 1/24/17.
+ */
+public class UserList extends Activity {
+    ListView mListView;
+    String[] prenoms = new String[]{
+            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
+            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
+            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
+            "Sophie", "Tristan", "Ulric", "Vincent", "Willy", "Xavier",
+            "Yann", "Zoé"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_list);
+        mListView = (ListView) findViewById(R.id.listView);
+
+        //android.R.layout.simple_list_item_1 est une vue disponible de base dans le SDK android,
+        //Contenant une TextView avec comme identifiant "@android:id/text1"
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(UserList.this,
+                android.R.layout.simple_list_item_1, prenoms);
+        mListView.setAdapter(adapter);
+    }
+}
